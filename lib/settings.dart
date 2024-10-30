a418ea15c08dd1a5108b1c8366b05d905a3f7164
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-10-29 21:59:32 -0600

    Build: empezando la pagina de ajustes o settings o como se llame

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
@@ -7,13 +8,24 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Back to Home Page'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(height: 40),  // Espacio en la parte superior
+          SvgPicture.asset(
+            'assets/usuario.svg',
+            height: 250,  // Ajusta el tamaño de la imagen según tus necesidades
+          ),
+          const SizedBox(height: 20),  // Espacio entre la imagen y el botón
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back to Home Page'),
+            ),
+          ),
+        ],
       ),
     );
   }
